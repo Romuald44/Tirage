ff279ccc87aa0bab3f1624c532c37051dc397c25
Romuald Le Brun <Romuald Le Brun>
2016-01-11 20:39:50 +0100

    Essaye pour mettre les random dans un tableau mais pas fini

--- src/tirage/Tirage.java
+++ src/tirage/Tirage.java
@@ -14,32 +14,50 @@ public class Tirage {
     private static Fenetre frame;
        
     public static void main(String[] args) {
-        frame = new Fenetre();
+        //frame = new Fenetre();
+        selection();
     }
     
     public static String selection() {
-        int animateur = random();
+        int tab_pers[] = null;
+        int pre_random=0;
+        int act_random=0;
+        /*int animateur = random();
         int secretaire = random();
         int scribe = random();
-        int gestionnaire = random();
+        int gestionnaire = random();*/
         
         //System.out.println("Animateur => "+nom(animateur));
         
-        while(secretaire == animateur) {
-            secretaire = random();
+        for(int i=0; i<4; i++) {
+            pre_random = random();
+            
+            if(i>=1) {
+                act_random = random();
+            }
+            
+            if(act_random != pre_random) {
+                tab_pers[i] = act_random;
+                System.out.println(tab_pers[i]);
+            }
         }
+        
+        return "";
+        /*while(secretaire == animateur) {
+            secretaire = random();
+        }*/
         //System.out.println("Secrétaire => "+nom(secretaire));
         
-        while(scribe == animateur || scribe == secretaire) {
+       /* while(scribe == animateur || scribe == secretaire) {
             scribe = random();
-        }
+        }*/
         //System.out.println("Scribe => "+nom(scribe));
         
-        while(gestionnaire == animateur || gestionnaire == secretaire || gestionnaire == scribe) {
+        /*while(gestionnaire == animateur || gestionnaire == secretaire || gestionnaire == scribe) {
             gestionnaire = random();
-        }
+        }*/
         //System.out.println("Gestionnaire => "+nom(gestionnaire));
-        return "Animateur => "+nom(animateur)+"\n"+"Secrétaire => "+nom(secretaire)+"\n"+"Scribe => "+nom(scribe)+"\n"+"Gestionnaire => "+nom(gestionnaire);
+        //return "Animateur => "+nom(animateur)+"\n"+"Secrétaire => "+nom(secretaire)+"\n"+"Scribe => "+nom(scribe)+"\n"+"Gestionnaire => "+nom(gestionnaire);
     }
     
     public static int random() {
